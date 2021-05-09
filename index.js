a44fa7d7d3be24d9053335a8bc89971e7c69c84d
vidocco <jack.mk.thibaut@gmail.com>
2021-05-09 18:33:04 +0200

    refactor: initialize db on server init

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const Koa        = require('koa')
 const app        = new Koa()
-const serve      = require('koa-static')
 const router     = require('./router.js')
 const bodyParser = require('koa-bodyparser')
 const cors       = require('@koa/cors')
 
+const db_init = require("./mongo")
+
 require('dotenv').config()
 
-const { PORT = 3000, HOST = "http://localhost" } = process.env
+const {
+  PORT = 3000,
+  HOST = 'http://localhost'
+} = process.env
 
-const options = {
-  origin: 'http://listmera.rocks',
+const corsOptions = {
+  origin: '*',
 }
 
+db_init()
+
 app.use(bodyParser())
-  .use(cors(options))
+  .use(cors(corsOptions))
   .use(router.routes())
   .use(router.allowedMethods())
 
